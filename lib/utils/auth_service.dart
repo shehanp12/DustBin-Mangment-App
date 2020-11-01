3b05e768b95d8ea1f8d6c3fd2fa0f0391329b44c
shehanp12 <shehanp068@gmail.com>
2020-11-01 15:49:56 +0530

    sigin in

--- lib/utils/auth_service.dart
+++ lib/utils/auth_service.dart
@@ -1,6 +1,23 @@
-
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService{
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+
+
+
+  // sign in with email and password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+      return user;
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
 
 
 
